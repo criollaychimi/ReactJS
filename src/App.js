9e38b8a5fe24480efd41e41ac5304d71795e2dd6
vilual10 <vilual10@gmail.com>
2021-12-01 19:37:49 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tienda virtual de Bizarrap
         </a>
       </header>
     </div>
