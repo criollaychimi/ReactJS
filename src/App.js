bb3cac6459d2f8b0b212934a7c18ec402818365f
vilual10 <vilual10@gmail.com>
2021-12-06 23:25:37 -0300

    2docambio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tienda virtual de Bizarrap
-        </a>
+        <NavBar />
+        
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+    function NavBar() {
+        return (
+            <nav className="navBarStyle">
+                <div className="navBarItemsStyle">INICIO</div>
+                <div className="navBarItemsStyle">GORRAS</div>
+                <div className="navBarItemsStyle">REMERAS</div>
+                <div className="navBarItemsStyle">BUZOS</div>
+            </nav>
+        
+        ) 
+        
+    }
+
+export default NavBar;
