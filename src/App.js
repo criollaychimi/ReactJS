1b419c7532f3b39f43bc746531c8abfffc6fb31f
vilual10 <vilual10@gmail.com>
2021-12-13 23:57:46 -0300

    components 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import CartWidget from './components/CartWidget';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <NavBar />
         <CartWidget />
         <ItemListContainer />
-
+        <ItemCount></ItemCount>
         
       </header>
     </div>
---
+++ src/components/ItemCount.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function ItemCount() {
+
+const [itemCount, setItemCount] = React.useState(1);
+
+return (
+	<div style={{ display: "block", padding: 30 }}>
+	
+	<div>
+		<div color="secondary" badgeContent={itemCount}>
+		<img src="../media/carts.png" />{" "}
+		
+			onClick={() => {
+			setItemCount(Math.max(itemCount - 1, 0));
+			}}
+		>
+			{" "}
+			<img src="../media/minus.png" fontSize="small" />
+		
+		
+			onClick={() => {
+			setItemCount(itemCount + 1);
+			}}
+		
+			{" "}
+			<img src="../media/plus.png" fontSize="small" />
+		</div>
+
+	</div>
+	</div>
+);
+}
+
+export default ItemCount;
